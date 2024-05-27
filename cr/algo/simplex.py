b9ab5cef12c67c153133a551a6c6568353556064
manelx2 <benhassinem02@gmail.com>
2024-05-27 13:28:30 +0100

    manel

--- cr/algo/simplex.py
+++ cr/algo/simplex.py
@@ -1,188 +1,64 @@
 import heapq
+import numpy as np
 
+def simplex(c, A, b,z):
+    m, n = A.shape
+    tableau = np.zeros((m+1, n+m+1))
+    tableau[:-1, :-1] = np.hstack((A, np.eye(m)))
+    tableau[:-1, -1] = b
+    # Ajustement de la dimension de c ici
+    c_adjusted = np.zeros(n+m)
+    c_adjusted[:len(c)] = -c
+    tableau[-1, :-1] = c_adjusted
+    tableau[-1, -1] = 0
 
-def identity(numRows, numCols, val=1, rowStart=0):
-   return [[(val if i == j else 0) for j in range(numCols)]
-               for i in range(rowStart, numRows)]
+    while np.any(tableau[-1, :-1] < 0):
+        entering_variable = np.argmin(tableau[-1, :-1])
+        if np.all(tableau[:-1, entering_variable] <= 0):
+            raise Exception('Le programme n\'a pas de solution finie.')
+        ratios = tableau[:-1, -1] / tableau[:-1, entering_variable]
+        leaving_variable = np.argmin(ratios[ratios > 0])
+        
+        pivot = tableau[leaving_variable, entering_variable]
+        tableau[leaving_variable, :] /= pivot
+        for i in range(m+1):
+            if i != leaving_variable:
+                ratio = tableau[i, entering_variable]
+                tableau[i, :] -= ratio * tableau[leaving_variable, :]
+    
+    variables_decision = tableau[:-1, -1]
+    solution = {
+      'valeur_objectif': tableau[-1, -1],
+      'tableau_simplexe': tableau
+    }
+    for i in range(n):
+        solution[f'variable_decision_{i+1}'] = variables_decision[i]
+    if(z=="max"):
+      solution['valeur_objectif']=-solution['valeur_objectif']
+    
+    return solution
 
+def convert_standard_pl(c,A,b,z,op):
+  if z == "max":
+        return A, b, c
+  else:
+        c = -c  # Inverser les coefficients de la fonction objectif pour la minimisation
+  for i in range(3):
+        if op[i] == ">=" or op[i] == ">":
+            A[i, :] = -A[i, :]  # Inverser les coefficients des contraintes
+            b[i] = -b[i]  # Inverser les valeurs des contraintes
+  return A,b,c
 
-
-def standardForm(cost, greaterThans=[], gtThreshold=[], lessThans=[], ltThreshold=[],
-               equalities=[], eqThreshold=[], maximization=True):
-   newVars = 0
-   numRows = 0
-   if gtThreshold != []:
-      newVars += len(gtThreshold)
-      numRows += len(gtThreshold)
-   if ltThreshold != []:
-      newVars += len(ltThreshold)
-      numRows += len(ltThreshold)
-   if eqThreshold != []:
-      numRows += len(eqThreshold)
-
-   if not maximization:
-      cost = [-x for x in cost]
-
-   if newVars == 0:
-      return cost, equalities, eqThreshold
-
-   newCost = list(cost) + [0] * newVars
-
-   constraints = []
-   threshold = []
-
-   oldConstraints = [(greaterThans, gtThreshold, -1), (lessThans, ltThreshold, 1),
-                     (equalities, eqThreshold, 0)]
-
-   offset = 0
-   for constraintList, oldThreshold, coefficient in oldConstraints:
-      constraints += [c + r for c, r in zip(constraintList,
-         identity(numRows, newVars, coefficient, offset))]
-
-      threshold += oldThreshold
-      offset += len(oldThreshold)
-
-   return newCost, constraints, threshold
-
-
-def dot(a,b):
-   return sum(x*y for x,y in zip(a,b))
-
-def column(A, j):
-   return [row[j] for row in A]
-
-def transpose(A):
-   return [column(A, j) for j in range(len(A[0]))]
-
-def isPivotCol(col):
-   return (len([c for c in col if c == 0]) == len(col) - 1) and sum(col) == 1
-
-def variableValueForPivotColumn(tableau, column):
-   pivotRow = [i for (i, x) in enumerate(column) if x == 1][0]
-   return tableau[pivotRow][-1]
-
-# assume the last m columns of A are the slack variables; the initial basis is
-# the set of slack variables
-def initialTableau(c, A, b):
-   tableau = [row[:] + [x] for row, x in zip(A, b)]
-   tableau.append([ci for ci in c] + [0])
-   return tableau
-
-
-def primalSolution(tableau):
-   # the pivot columns denote which variables are used
-   columns = transpose(tableau)
-   indices = [j for j, col in enumerate(columns[:-1]) if isPivotCol(col)]
-   return [(colIndex, variableValueForPivotColumn(tableau, columns[colIndex]))
-            for colIndex in indices]
-
-
-def objectiveValue(tableau):
-   return -(tableau[-1][-1])
-
-
-def canImprove(tableau): 
-   lastRow = tableau[-1]
-   return any(x > 0 for x in lastRow[:-1])
-
-
-# this can be slightly faster
-def moreThanOneMin(L):
-   if len(L) <= 1:
-      return False
-
-   x,y = heapq.nsmallest(2, L, key=lambda x: x[1])
-   return x == y
-
-
-def findPivotIndex(tableau):
-   # pick minimum positive index of the last row
-   column_choices = [(i,x) for (i,x) in enumerate(tableau[-1][:-1]) if x > 0]
-   column = min(column_choices, key=lambda a: a[1])[0]
-
-   # check if unbounded
-   if all(row[column] <= 0 for row in tableau):
-      raise Exception('Linear program is unbounded.')
-
-   # check for degeneracy: more than one minimizer of the quotient
-   quotients = [(i, r[-1] / r[column])
-      for i,r in enumerate(tableau[:-1]) if r[column] > 0]
-
-   if moreThanOneMin(quotients):
-      raise Exception('Linear program is degenerate.')
-
-   # pick row index minimizing the quotient
-   row = min(quotients, key=lambda x: x[1])[0]
-
-   return row, column
-
-
-def pivotAbout(tableau, pivot):
-   i,j = pivot
-
-   pivotDenom = tableau[i][j]
-   tableau[i] = [x / pivotDenom for x in tableau[i]]
-
-   for k,row in enumerate(tableau):
-      if k != i:
-         pivotRowMultiple = [y * tableau[k][j] for y in tableau[i]]
-         tableau[k] = [x - y for x,y in zip(tableau[k], pivotRowMultiple)]
-
-
-def simplex(c, A, b):
-   tableau = initialTableau(c, A, b)
-   print("Initial tableau:")
-   for row in tableau:
-      print(row)
-   print()
-
-   while canImprove(tableau):
-      pivot = findPivotIndex(tableau)
-      print("Next pivot index is=%d,%d \n" % pivot)
-      pivotAbout(tableau, pivot)
-      print("Tableau after pivot:")
-      for row in tableau:
-         print(row)
-      print()
-
-   return tableau, primalSolution(tableau), objectiveValue(tableau)
-
-
-
-def collect_LP_from_user():
-   # Prompt the user to enter the number of variables and constraints
-   num_variables = int(input("Enter the number of variables: "))
-   num_constraints = int(input("Enter the number of constraints: "))
-
-   # Prompt the user to enter the coefficients of the objective function
-   print("Enter the coefficients of the objective function:")
-   c = [float(input(f"c[{i}]: ")) for i in range(num_variables)]
-
-   # Prompt the user to enter the constraint matrix
-   print("Enter the constraint matrix A (one row at a time):")
-   A = [[float(input(f"A[{i}][{j}]: ")) for j in range(num_variables)] for i in range(num_constraints)]
-
-   # Prompt the user to enter the right-hand side vector
-   print("Enter the right-hand side vector b:")
-   b = [float(input(f"b[{i}]: ")) for i in range(num_constraints)]
-
-   return c, A, b,num_constraints
-
-if __name__ == "__main__":
-   # Collect the linear program from the user
-   c, A, b ,num_constraints= collect_LP_from_user()
-
-   # add slack variables by hand
-   num_slack_variables = num_constraints
-   for row in A:
-      row += [0] * num_slack_variables
-      c += [0] * num_slack_variables
-
-   # Solve the linear program using the simplex algorithm
-   t, s, v = simplex(c, A, b)
-
-   # Print the primal solution and the value of the objective function
-   print("Primal Solution:")
-   for variable, value in s:
-      print(f"x[{variable}] = {value}")
-   print("Objective Value:", v)
+  
+# Exemple d'utilisation
+z = "min"
+op=["<=",">=","<="]
+c = np.array([-2, -3])  # Coefficients de la fonction objectif
+A = np.array([[1,6], [-2,-2], [4,1]])  # Coefficients des contraintes
+b = np.array([30, -15, 24])  # Valeurs des contraintes
+A,b,c=convert_standard_pl(c,A,b,z,op)
+resultat = simplex(c, A, b,z)
+print("Solution optimale:")
+print("Valeur de la fonction objectif:", resultat['valeur_objectif'])
+for i in range(2):
+    print(f"Valeur de la variable de décision {i+1}: {resultat[f'variable_decision_{2-i}']}")
